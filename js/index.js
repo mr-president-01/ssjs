8e814c75f21ee9f66ad3463689f81a42549a085d
mr-president-01 <volodymyrherych22@gmail.com>
2019-02-04 16:37:24 +0200

    js03

--- js/index.js
+++ js/index.js
@@ -2,28 +2,28 @@ const n = parseFloat(prompt('Enter a serial number of fibonacci'));
 
 // cycle
 
-// function fiboLoop(n){
-//     let first = 1;
-//     let second =1;
-//     for (let i = 3; i <= n; i++){
-//         let next = first + second;
-//         first = second;
-//         second = next;
-//     }
-//     return second;
-// };
-// alert(fiboLoop(n));
+function fiboLoop(n){
+    let first = 1;
+    let second =1;
+    for (let i = 3; i <= n; i++){
+        let next = first + second;
+        first = second;
+        second = next;
+    }
+    return second;
+};
+alert(fiboLoop(n));
 
 // recursion 
 
-// function fiboRec(n){
-//     if (n <= 1){
-//         return n;
-//     } else {
-//         return fiboRec(n-1)+fiboRec(n-2);
-//     }
-// }
-// alert(fiboRec(n));
+function fiboRec(n){
+    if (n <= 1){
+        return n;
+    } else {
+        return fiboRec(n-1)+fiboRec(n-2);
+    }
+}
+alert(fiboRec(n));
 
 // array
 
