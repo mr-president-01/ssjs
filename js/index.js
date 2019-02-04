a6b66164f55653e34a1dec60086070a6d1c1501c
mr-president-01 <volodymyrherych22@gmail.com>
2019-02-04 16:06:01 +0200

    js02

---
+++ js/index.js
@@ -0,0 +1,38 @@
+const n = parseFloat(prompt('Enter a serial number of fibonacci'));
+
+// cycle
+
+// function fiboCycle(n){
+//     let first = 1;
+//     let second =1;
+//     for (let i = 3; i <= n; i++){
+//         let next = first + second;
+//         first = second;
+//         second = next;
+//     }
+//     return second;
+// };
+// alert(fibo(n));
+
+// recursion 
+
+// function fiboRec(n){
+//     if (n <= 1){
+//         return n;
+//     } else {
+//         return fiboRec(n-1)+fiboRec(n-2);
+//     }
+// }
+// alert(fiboRec(n));
+
+// array
+
+function fiboArr(n){
+    arr = [1,1];
+    for (let i = 3; i <= n; i++){
+        arr.push(arr[i-2]+arr[i-3]);
+    }
+    return arr.pop();
+}
+
+alert(fiboArr(n));
